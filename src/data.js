ea52ea12aaf587e0775a6d383bfbd6cf59e577d6
Jocelyn <129541722+JoceChile@users.noreply.github.com>
2023-05-19 14:14:20 -0400

    vivie consulta

--- src/data.js
+++ src/data.js
@@ -1,9 +1,41 @@
+export default dataGhibli;
+const todoGhibli = Object.values(dataGhibli.films);
+const busqueda = document.getElementById("busqueda");//donde escribo input
+const botonBusqueda = document.getElementById("boton-buscar");//click buscar
+const contenedorBusqueda = document.getElementById("contenedor-busqueda");
+const buscar = () => {
+    const texto = busqueda.value()
+    contenedorBusqueda.innerHTML = "";
+    for (let film of films) {
+        let films = film.nombre
+        if (films.indexOf(texto) !== -1) {
+            contenedorBusqueda.innerHTML += `
+            <div class="afiche">
+            <h1>${film.title}</h1>
+            
+            </div>
+            `;
+        }
+    } 
+    if (contenedorBusqueda.innerHTML === "") {
+        contenedorBusqueda.innerHTML += `
+        alert "No encontrado"
+        `
+    }
+    })
+
+}
+
+botonBusqueda.addEventListener("click", buscar);
+
+
+//aca debo insertar filter data / sortData / computeStats
+//manipulacion de arreglos y objetos
+
 // estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const informacion = () => {
+//   return 'informacion';
+// };
+// export const anotherExample = () => {
+//   return 'OMG';
+// }; */}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,78 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import dataGhibli from './data/ghibli/ghibli.js';
+//console.log(dataGhibli.films);
+const todoGhibli = Object.values(dataGhibli.films);
+// console.log(todoGhibli);
 
-console.log(example, data);
+const contenedorBusqueda = document.getElementById("contenedor-busqueda");
+const botonPeliculas = document.getElementById("mostrar-peliculas");
+const botonPersonajes = document.getElementById("mostrar-personajes");
+const botonLocaciones = document.getElementById("mostrar-locaciones");
+const botonVehiculos = document.getElementById("mostrar-vehiculos");
+
+const peliculas = todoGhibli.filter((element) => {
+    return element.title;
+});
+
+const personajes = todoGhibli.filter((element) => {
+    return element.people[0];
+}) 
+
+const locaciones = todoGhibli.filter((element) => {
+    return element.locations;
+});
+
+const vehiculos = todoGhibli.filter((element) => {
+     return element.vehicles;
+});
+
+botonPeliculas.addEventListener("click", function () {
+    contenedorBusqueda.innerHTML = "";
+    peliculas.forEach((title) => {
+        contenedorBusqueda.innerHTML += `
+        <div class="afiche">
+        <img src="${title.poster}"><br>
+        <h1>${title.title}</h1>
+        <p>${title.rt_score}</p>
+        </div>
+        `;
+    });
+});
+
+botonPersonajes.addEventListener("click", function () {
+    contenedorBusqueda.innerHTML = "";
+    personajes.forEach((people) => {
+        contenedorBusqueda.innerHTML += `
+        <div class="afiche">
+        <img src="${people.img}"><br>
+        <h1>${people.name}</h1>
+        <p>${people.age}</p>
+        </div>
+        `;
+    });
+});
+
+botonLocaciones.addEventListener("click", function () {
+    contenedorBusqueda.innerHTML = "";
+    locaciones.forEach((locations) => {
+        contenedorBusqueda.innerHTML += `
+        <div class="afiche">
+        <img src="${locations.img}"><br>
+        <h1>${locations.name}</h1>
+        <p>${locations.climate}</p>
+        </div>
+        `;
+    });
+});
+
+botonVehiculos.addEventListener("click", function () {
+    contenedorBusqueda.innerHTML = "";
+    vehiculos.forEach((vehicles) => {
+        contenedorBusqueda.innerHTML += `
+        <div class="afiche">
+        <img src="${vehicles.img}"><br>
+        <h1>${vehicles.name}</h1>
+        <p>${vehicles.vehicle_class}</p>
+        </div>
+        `;
+    });
+});
