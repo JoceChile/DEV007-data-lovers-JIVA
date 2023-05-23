7279e22573b6f24474cc527b11e6be4a3287077c
Jocelyn <129541722+JoceChile@users.noreply.github.com>
2023-05-22 20:01:39 -0400

    sigue sin funcionar ordenar

--- src/data.js
+++ src/data.js
@@ -1,41 +1,19 @@
-export default dataGhibli;
-const todoGhibli = Object.values(dataGhibli.films);
-const busqueda = document.getElementById("busqueda");//donde escribo input
-const botonBusqueda = document.getElementById("boton-buscar");//click buscar
-const contenedorBusqueda = document.getElementById("contenedor-busqueda");
-const buscar = () => {
-    const texto = busqueda.value()
-    contenedorBusqueda.innerHTML = "";
-    for (let film of films) {
-        let films = film.nombre
-        if (films.indexOf(texto) !== -1) {
-            contenedorBusqueda.innerHTML += `
-            <div class="afiche">
-            <h1>${film.title}</h1>
-            
-            </div>
-            `;
-        }
-    } 
-    if (contenedorBusqueda.innerHTML === "") {
-        contenedorBusqueda.innerHTML += `
-        alert "No encontrado"
-        `
-    }
-    })
 
+//escribir export antes de function
+
+export function ordenar(order, peliculas) {
+    peliculas.sort(function (a, b) {
+        const titleA = a.title.toUpperCase();
+        const titleB = b.title.toUpperCase();
+        if (order === "a-z") {
+            if (titleA < titleB) return -1;
+            if (titleA > titleB) return 1;
+        } else if (order === "z-a") {
+            if (titleA > titleB) return -1;
+            if (titleA < titleB) return 1;
+        } 
+        return 0;
+    });
 }
 
-botonBusqueda.addEventListener("click", buscar);
-
-
-//aca debo insertar filter data / sortData / computeStats
-//manipulacion de arreglos y objetos
-
-// estas funciones son de ejemplo
-// export const informacion = () => {
-//   return 'informacion';
-// };
-// export const anotherExample = () => {
-//   return 'OMG';
-// }; */}
+// quiero ordenar de la a-z lo que aparezca en el contenedor busqueda
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import dataGhibli from './data/ghibli/ghibli.js';
 //console.log(dataGhibli.films);
+import { ordenar } from './data.js';
 const todoGhibli = Object.values(dataGhibli.films);
 // console.log(todoGhibli);
+
+// import { ordenar } from './data.js'; 
+// // ordenar (peliculas.title);
+
+// import { selectElement } from './data.js';
 
 const contenedorBusqueda = document.getElementById("contenedor-busqueda");
 const botonPeliculas = document.getElementById("mostrar-peliculas");
@@ -14,7 +20,7 @@ const peliculas = todoGhibli.filter((element) => {
 });
 
 const personajes = todoGhibli.filter((element) => {
-    return element.people[0];
+    return element.people;
 }) 
 
 const locaciones = todoGhibli.filter((element) => {
@@ -31,48 +37,92 @@ botonPeliculas.addEventListener("click", function () {
         contenedorBusqueda.innerHTML += `
         <div class="afiche">
         <img src="${title.poster}"><br>
-        <h1>${title.title}</h1>
+        <h2>${title.title}</h2>
+        <p>${title.description}</p>
+        <h3>${title.director}</h3>
+        <h3>${title.producer}</h3>
+        <p>${title.release_date}</p>
         <p>${title.rt_score}</p>
         </div>
         `;
-    });
+    }); 
 });
+
+const selectElement = document.getElementById("ordenar-select");
+selectElement.addEventListener("change", function () {
+    const selectedOption = selectElement.value;
+    const dataOrdenada = ordenar(selectedOption, peliculas); //llamar del main
+    contenedorBusqueda.innerHTML = "";
+    dataOrdenada.forEach((title) => {
+        contenedorBusqueda.innerHTML += `
+        <div class="afiche">
+        <img src="${title.poster}"><br>
+        <h2>${title.title}</h2>
+        <p>${title.description}</p>
+        <h3>${title.director}</h3>
+        <h3>${title.producer}</h3>
+        <p>${title.release_date}</p>
+        <p>${title.rt_score}</p>
+        </div>
+        `;
+      });
+    });
+
 
 botonPersonajes.addEventListener("click", function () {
     contenedorBusqueda.innerHTML = "";
-    personajes.forEach((people) => {
-        contenedorBusqueda.innerHTML += `
-        <div class="afiche">
-        <img src="${people.img}"><br>
-        <h1>${people.name}</h1>
-        <p>${people.age}</p>
-        </div>
-        `;
+    personajes.forEach((film) => {
+        film.people.forEach(people => {
+            contenedorBusqueda.innerHTML += `
+            <div class="afiche">
+            <img src="${people.img}"><br>
+            <h2>${people.name}</h2>
+            <h3>${people.gender}</h3>
+            <h3>${people.age}</h3>
+            <h3>${people.eye_color}</h3>
+            <h3>${people.hair_color}</h3>
+            <p>${people.specie}</p>
+            </div>
+            `;  
+        })
     });
 });
+
 
 botonLocaciones.addEventListener("click", function () {
     contenedorBusqueda.innerHTML = "";
-    locaciones.forEach((locations) => {
+    locaciones.forEach((film) => {
+        film.locations.forEach(locations => {
         contenedorBusqueda.innerHTML += `
         <div class="afiche">
         <img src="${locations.img}"><br>
-        <h1>${locations.name}</h1>
-        <p>${locations.climate}</p>
+        <h2>${locations.name}</h2>
+        <h3>${locations.climate}</h3>
+        <h3>${locations.terrain}</h3>
+        <h3>${locations.surface_water}</h3>
         </div>
         `;
+        })
+    })
     });
-});
+//en locaciones no pude agregar residentes de los lugares
 
 botonVehiculos.addEventListener("click", function () {
     contenedorBusqueda.innerHTML = "";
-    vehiculos.forEach((vehicles) => {
+    vehiculos.forEach((film) => {
+        film.vehicles.forEach(vehicles => {
         contenedorBusqueda.innerHTML += `
         <div class="afiche">
         <img src="${vehicles.img}"><br>
-        <h1>${vehicles.name}</h1>
-        <p>${vehicles.vehicle_class}</p>
+        <h2>${vehicles.name}</h2>
+        <p>${vehicles.description}</p>
+        <h3>${vehicles.vehicle_class}</h3>
+        <h3>${vehicles.length}</h3>
+        <h3>${vehicles.pilot.name}</h3>
         </div>
         `;
+        })
     });
 });
+
+
